9be6b2308ca11f6f8e16e8a323142f27877b145e
Scrapbandit <valeriamoo@gmail.com>
2022-03-03 12:46:13 +0100

    Using arrays
    pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 let pokemonList = [{
 
-}]
+  name:'Bulbasaur'
+  Height: 2' 04",
+  type: ['grass', 'poison']
+
+},
+
+{
+  name: 'Charmander'
+  Height: 2,00",
+  type: ['fire']
+
+},
+
+{
+name: 'Squirtle'
+Height: 1' 08",
+type: ['water']
+
+}
+];
