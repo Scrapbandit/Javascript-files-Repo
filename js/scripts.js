529707cff047fa795c5b2498f17ff634052973a1
Scrapbandit <valeriamoo@gmail.com>
2022-03-06 23:26:16 +0100

    errors fix
    Unexpected identifier

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@
 let pokemonList = [{
 
   name:'Charizard'
-  Height: 5.07,
+  height: 5.07,
   type: ['fire', 'flying']
 
 },
 
 {
   name: 'pikachu'
-  Height: 1.04,
+  height: 1.04,
   type: ['electric']
 
 },
 
 {
 name: 'Bulbasaur'
-Height: 2.04,
+height: 2.04,
 type: ['grass', 'poison']
 
 }
@@ -25,6 +25,7 @@ type: ['grass', 'poison']
 
 //loops
   // for (let i = 0; i < pokemonList.length; i++) {
-
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i] + "<br>");
 
 }
