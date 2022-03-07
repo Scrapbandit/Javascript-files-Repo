6fb75f055bf9a5a92fcf024910856fb82be852c7
Scrapbandit <valeriamoo@gmail.com>
2022-03-07 12:42:10 +0100

    correcting the loop
    with condition
    task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,18 @@ let pokemonList = [{
   name:'Charizard'
   height: 5.07,
   type: ['fire', 'flying']
-
 },
 
 {
   name: 'pikachu'
   height: 1.04,
   type: ['electric']
-
 },
 
 {
 name: 'Bulbasaur'
 height: 2.04,
 type: ['grass', 'poison']
-
 }
 ];
 
@@ -28,7 +25,12 @@ type: ['grass', 'poison']
   // for (let i = 0; i < pokemonList.length; i++) {
   //}
   for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + '</p>');
+    if (pokemon.height > 5) {
+    //  console.log('WOW! this Pokemon is big')
+    document.write(pokemonList[i].name + '(height : ' + pokemonList[i].height +' )' + 'WOW! this Pokemon is big!' </br>)
+else {
+  document.write (pokemonList[i].name + 'height : ' + pokemonList[i].height +' ) </br>')
+}
 //what to insert in the document.write
 }
 //loops option 2 (second try)
