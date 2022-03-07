c956fbf8fb49d326c196d2c3ebeef948ea51b643
Scrapbandit <valeriamoo@gmail.com>
2022-03-07 10:26:52 +0100

    adding comments to the loops script JS
    //

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,21 @@ type: ['grass', 'poison']
 }
 ];
 
-//loops
+//loops for pokemon list option 1 scheme
+  // the formular
   // for (let i = 0; i < pokemonList.length; i++) {
+  //}
   for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i] + "<br>");
+//what to insert in the document.write
+
+//loops option 2 (second try)
+
+
+  for(var i = 3; i >= 0; i--){
+  if(i == 0){
+    document.write(<p>'name' + 'height' .</p>')
+  }
+}
 
 }
