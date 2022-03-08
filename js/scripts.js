0829d485fcf628d03d8a1f38394cb60be4578c51
Scrapbandit <valeriamoo@gmail.com>
2022-03-08 15:37:44 +0100

    comma add js

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@
 
 let pokemonList = [{
 
-  name:'Charizard'
+  name:'Charizard',
   height: 5.07,
   type: ['fire', 'flying']
 },
 
 {
-  name: 'pikachu'
+  name: 'pikachu',
   height: 1.04,
   type: ['electric']
 },
 
 {
-name: 'Bulbasaur'
+name: 'Bulbasaur',
 height: 2.04,
 type: ['grass', 'poison']
 }
@@ -25,11 +25,11 @@ type: ['grass', 'poison']
   // for (let i = 0; i < pokemonList.length; i++) {
   //}
   for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemon.height > 5) {
+    if (pokemonList.height > 5) {
     //  console.log('WOW! this Pokemon is big')
-    document.write(pokemonList[i].name + '(height : ' + pokemonList[i].height +' )' + 'WOW! this Pokemon is big!' </br>)
-else {
-  document.write (pokemonList[i].name + 'height : ' + pokemonList[i].height +' ) </br>')
+    document.write(pokemonList[i].name + '(height : ' + pokemonList[i].height  + 'WOW! this Pokemon is big! </br>');
+}else {
+  document.write (pokemonList[i].name + 'height : ' + pokemonList[i].height +'  </br>');
 }
 //what to insert in the document.write
 }
@@ -40,3 +40,20 @@ else {
   if(i == 0){
     document.write(<p>'name' + 'height' .</p>')
 non correct code */
+//contitions
+
+let pokemon= { name: 'Charizard', height: 5.07};
+
+if (pokemon.height > 5) {
+  console.log('WOW! this Pokemon is big');
+}
+
+function div(dividen, divisor){
+  if (divisor ===0){
+    return "you are dividing by zero."
+  }else {
+    let result = dividend/divisor;
+    return result;
+
+  }
+}
