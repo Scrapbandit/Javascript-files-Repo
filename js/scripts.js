74870a3517c8ee6df326178f185fabd4f266c92b
Scrapbandit <valeriamoo@gmail.com>
2022-03-14 18:54:37 +0100

    task 1.6
    update mission

--- js/scripts.js
+++ js/scripts.js
@@ -19,55 +19,43 @@ let pokemonList = [{
 name: 'Bulbasaur',
 height: 2.04,
 type: ['grass', 'poison']
-}
+},
 ];
 
-return {
-  add: function (pokemon) {
+function add(pokemon){
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "type" in pokemon
+    ) {
     pokemonList.push (pokemon);
-  },
-  getAll: function () {
+  }  else {
+        console.log("pokemon is not correct");
+      }
+  }
+function getAll () {
     return pokemonList;
   }
-};
+
+function addListItem(pokemon){
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listpokemon = document.createElement("li");
+  let button = document.createElement ("button");
+  button.innerText = pokemon.name;
+  listpokemon.appendChild(button);
+  pokemonList.appendChild(listpokemon);
+}
+return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem
+ };
 })();
 
-//.foreach() task 1.5 (updated)
-pokemonRepository.getAll().forEach(function(pokemon){
-document.write(pokemon.name +" "+ "(height)" +" "+ pokemon.height + " "+ "type:" + " " + pokemon.type);
-document.write ( '<p/>');
-});
-
 // add() function
+pokemonRepository.add({name: "Mew", height: 1.5, type: ["Psychic"] });
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: 'Mew'});
-console.log(pokemonRepository.getAll());
-
-
-
-
-
-
-
-
-
-/*
-//loops for pokemon list option 1 scheme
-
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 5) {
-    //  console.log('WOW! this Pokemon is big')
-    document.write(pokemonList[i].name + '(height : ' + pokemonList[i].height  +')' + 'WOW! this Pokemon is big! </br>')
-  }
-    else {
-  document.write (pokemonList[i].name + '(height : ' + pokemonList[i].height +')  </br>')
-}
-} */
-
-/*
-let pokemon= { name: 'Charizard', height: 5.07};
-
-if (pokemon.height > 5) {
-  console.log('WOW! this Pokemon is big');
-}
-*/
+pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.addListItem(pokemon);
+});
