468a372d00351765fa6091533683ca487c3d7d81
Scrapbandit <valeriamoo@gmail.com>
2022-03-15 13:53:25 +0100

    last corrections
    task 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -46,13 +46,16 @@ function addListItem(pokemon){
   let listpokemon = document.createElement("li");
   let button = document.createElement ("button"); // create button
   button.innerText = pokemon.name;
+  button.classList.add("button-class");
   listpokemon.appendChild(button);
   pokemonList.appendChild(listpokemon);
   button.addEventListener("click", function(){ //addEventListener
-  function showDetails(pokemon);
+  showDetails(pokemon);
   })
 }
-
+function showDetails(pokemon){
+  console.log(pokemon);
+}
 return {
    add: add,
    getAll: getAll,
