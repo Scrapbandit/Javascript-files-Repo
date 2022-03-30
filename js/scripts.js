ae17af02a7ae90e13158549b61b5eb6c4aff5f68
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-03-30 18:23:58 +0300

    works

--- js/scripts.js
+++ js/scripts.js
@@ -31,8 +31,6 @@ let pokemonRepository = (function () {
     button.innerText = pokemon.name;
     // button.classList.add("button-class");
     button.classList.add('btn-outline-dark');
-    button.setAttribute("data-toggle", "modal");
-    button.setAttribute("data-target", "#pokemonModal");
     listpokemon.classList.add('group-list-item');
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
@@ -74,67 +72,41 @@ let pokemonRepository = (function () {
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       //adding showModal
-      showModal(pokemon.name, pokemon.height, pokemon.types, pokemon.imageUrl);
-      console.log("pokemon selected: " + pokemon.name + "is" + pokemon.height + "and with the abilities of" + pokemon.types);
-      //let modal = document.createElement('div');
-      //modal.classList.add('modal')
+      showModal(pokemon);
     });
   }
-//exercise 1.8 the modal container
 
 function showModal(item){
+  let modalContainer = document.querySelector('.modal-container');
   let modalBody = $('.modal-body');
      let modalTitle = $('.modal-title');
-     let modalHeader = $ ('.modal-header');
  
      modalTitle.empty();
      modalBody.empty();
  
      let nameElement = $('<h1>'+ item.name + '</h1>');
-     let imageElement = $('<img class="modal__img" style="width:50%">');
-    imageElement.attr("src", pokemon.imageUrl);
+     let imageElement = $('<img class="imageUrl" style="width:50%">');
+     imageElement.attr("src", item.imageUrl);
     
  
      let heightElement = $("<p>" + "height : " + item.height + "</p>");
      let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
-     let typetElement = $("<p>" + "type : " + item.type + "</p>");
+     let typeElement = $("<p>" + "type : " + item.type + "</p>");
  
      modalTitle.append(nameElement);
-     modalBody.append (imageElementFront);
-     modalBody.append (imageElementBack);
+     modalBody.append (imageElement);
      modalBody.append (heightElement);
      modalBody.append (weightElement);
      modalBody.append (typeElement);
 
-    let closeButton = document.querySelector(".modal-close");//changed from createElement to querySelector.
-    closeButton.addEventListener("click", hideModal);
-
-    window.addEventListener('keydown', (e) => {
-    console.log(e.key);
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible'))
-    hideModal();
-    });
-
-    //let titleElement = document.createElement ("h1");
-    //titleElement.innerText = title;
-
-    //modal.appendChild(closeButtonElement);
-    //modal.appendChild(titleElement);
-    //modal.appendChild(contentElement);
-    //modalContainer.appendChild(modal);
-
     modalContainer.classList.add("is-visible");
   }
 
-  function hideModal() {//payiing attention to type errors function with  small letters
-    let modalContainer = document.querySelector('.modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
+  function hideModal() {
+    $modalContainer.removeClass("is-visible");
+}
 
 
-/*  document.querySelector("#show-modal").addEventListener("click", ()=>{
-    showModal('Modal title', "This is the modal content!");
-  });*/
 
   return {
     add: add,
@@ -145,7 +117,6 @@ function showModal(item){
     showDetails: showDetails,
     showModal: showModal,
     hideModal: hideModal
-
   };
 })();
 
